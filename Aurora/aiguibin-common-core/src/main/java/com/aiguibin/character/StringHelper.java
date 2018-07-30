0b2beec90fa891230b9281f29283f072bae6f95d
AIguibin <aiguibin@163.com>
2018-07-30 09:50:43 +0800

    新增common-core代码

---
+++ Aurora/aiguibin-common-core/src/main/java/com/aiguibin/character/StringHelper.java
@@ -0,0 +1,36 @@
+package com.aiguibin.character;
+/**
+ * 描述： 字符串工具类
+ *
+ * @author AIguibin Date time 2018/7/25 11:23
+ */
+public class StringHelper {
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNull(String str){
+        //TODO
+        return true;
+    }
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isKong(String str){
+        //TODO
+        return true;
+    }
+    /**
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str){
+        //TODO
+        return true;
+    }
+}
