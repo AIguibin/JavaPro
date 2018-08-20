6384cc1807a70d53111c09aa88d8c53262fcf33f
AIguibin <aiguibin@163.com>
2018-08-20 14:47:17 +0800

    更新代码

--- Aurora/aiguibin-spring-jetty/src/main/java/com/aiguibin/jetty/config/JettyConfig.java
+++ Aurora/aiguibin-spring-jetty/src/main/java/com/aiguibin/jetty/config/JettyConfig.java
@@ -3,7 +3,6 @@ package com.aiguibin.jetty.config;
 import com.aiguibin.spring.config.SpringConfig;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -11,11 +10,9 @@ import org.springframework.context.annotation.Import;
  *
  * @author AIguibin Date time 2018/8/9 0:11
  */
-@Configuration
+
 @Import(SpringConfig.class)
 public class JettyConfig {
     private static final Log logger = LogFactory.getLog(JettyConfig.class);
-    public void te(){
-        logger.error("kkllll");
-    }
+
 }
