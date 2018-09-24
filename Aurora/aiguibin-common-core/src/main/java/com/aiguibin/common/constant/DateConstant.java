958d8235089053446111c39bb38449471e59fa27
AIguibin <aiguibin@163.com>
2018-09-25 01:09:26 +0800

    新增日期时间常量

---
+++ Aurora/aiguibin-common-core/src/main/java/com/aiguibin/common/constant/DateConstant.java
@@ -0,0 +1,36 @@
+package com.aiguibin.common.constant;
+
+public class DateConstant {
+    /**
+     * 常量定义：SEC - 秒
+     */
+    public static final long SEC = 1000;
+    /**
+     * 常量定义：MIN - 分
+     */
+    public static final long MIN = 60 * SEC;
+    /**
+     * 常量定义：HOUR - 小时
+     */
+    public static final long HOUR = 60 * MIN;
+    /**
+     * 常量定义：DAY - 天
+     */
+    public static final long DAY = 24 * HOUR;
+    /**
+     * 常量定义：WEEK - 周
+     */
+    public static final long WEEK = 7 * DAY;
+    /**
+     * 常量定义：MON - 月
+     */
+    public static final long MON = 30 * DAY;
+    /**
+     * 常量定义：QUAR - 季
+     */
+    public static final long QUAR = 3 * MON;
+    /**
+     * 常量定义：YEAR - 年
+     */
+    public static final long YEAR = 12 * MON;
+}
