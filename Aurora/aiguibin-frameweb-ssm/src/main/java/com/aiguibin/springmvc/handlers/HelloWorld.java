f0cf3694efbb5404ce6307d1d7519b18b7d302f8
AIguibin <aiguibin@163.com>
2019-01-18 15:30:42 +0800

    更换仓库名

---
+++ Aurora/aiguibin-frameweb-ssm/src/main/java/com/aiguibin/springmvc/handlers/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.aiguibin.springmvc.handlers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author AIguibin
+ */
+@Controller
+public class HelloWorld {
+    private static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+    //private static Log logger=LogFactory.getLog(HelloWorld.class);
+
+    /**
+     * @return
+     */
+    @RequestMapping("/helloworld")
+    public String hello(String str) {
+        System.out.println("hello");
+        str.substring(1, 5);
+        logger.error("log4j2日志");
+        return "success";
+    }
+}
--- Aurora/aiguibin-frameweb-ssm/src/main/webapp/index.jsp
+++ Aurora/aiguibin-frameweb-ssm/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="helloworld">我是超链接</a>
 </body>
 </html>
